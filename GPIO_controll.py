bcec7e760d1360dbda436a13ba59b446c6cec8f1
Felix Wegener <Felix.Wegener05@t-online.de>
2023-07-05 22:40:05 +0200

    add setmode gpios

--- GPIO_controll.py
+++ GPIO_controll.py
@@ -1,6 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BOARD)
+
+# GPIO.setup(led, GPIO.OUT)
+# GPIO.setup(switch, GPIO.IN)
+
 def changePinOut(pin, high):
     if (high):
         GPIO.output(pin, GPIO.HIGH)
